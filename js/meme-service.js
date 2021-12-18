f852ad82948bef57962bf4db6f3eca5843cebd87
TzviFraenkel <95023987+TzviFraenkel@users.noreply.github.com>
2021-12-18 20:22:31 +0200

    basic design and responsivity

--- js/meme-service.js
+++ js/meme-service.js
@@ -6,6 +6,20 @@ var gImgs = [
     { id: 2, url: 'img/2.jpg', keywords: ['funny', 'cat'] },
     { id: 3, url: 'img/3.jpg', keywords: ['funny', 'cat'] },
     { id: 4, url: 'img/4.jpg', keywords: ['funny', 'cat'] },
+    { id: 5, url: 'img/5.jpg', keywords: ['funny', 'cat'] },
+    { id: 6, url: 'img/6.jpg', keywords: ['funny', 'cat'] },
+    { id: 7, url: 'img/7.jpg', keywords: ['funny', 'cat'] },
+    { id: 8, url: 'img/8.jpg', keywords: ['funny', 'cat'] },
+    { id: 9, url: 'img/9.jpg', keywords: ['funny', 'cat'] },
+    { id: 10, url: 'img/10.jpg', keywords: ['funny', 'cat'] },
+    { id: 11, url: 'img/11.jpg', keywords: ['funny', 'cat'] },
+    { id: 12, url: 'img/12.jpg', keywords: ['funny', 'cat'] },
+    { id: 13, url: 'img/13.jpg', keywords: ['funny', 'cat'] },
+    { id: 14, url: 'img/14.jpg', keywords: ['funny', 'cat'] },
+    { id: 15, url: 'img/15.jpg', keywords: ['funny', 'cat'] },
+    { id: 16, url: 'img/16.jpg', keywords: ['funny', 'cat'] },
+    { id: 17, url: 'img/17.jpg', keywords: ['funny', 'cat'] },
+    { id: 18, url: 'img/18.jpg', keywords: ['funny', 'cat'] },
 ];
 var gMeme = {
     selectedImgId: 1,
@@ -17,7 +31,8 @@ var gMeme = {
             align: 'center',
             color: 'white',
             font: 'impact',
-            offsetY: 50 
+            stroke: true,
+            offsetY: 50
         }
     ]
 }
@@ -46,6 +61,22 @@ function alignChange(align) {
     gMeme.lines[lineIdx].align = align;
 }
 
+function changeFont(font) {
+    var lineIdx = gMeme.selectedLineIdx;
+    gMeme.lines[lineIdx].font = font;
+}
+
+function toggleStroke() {
+    var lineIdx = gMeme.selectedLineIdx;
+    gMeme.lines[lineIdx].stroke = !gMeme.lines[lineIdx].stroke;
+}
+function moveline(diff) {
+    var lineIdx = gMeme.selectedLineIdx;
+    if (gMeme.lines[lineIdx].offsetY + diff - gMeme.lines[lineIdx].size < 0
+        || gMeme.lines[lineIdx].offsetY + diff > getCanvasHeight()) return; //for now a bug because canvas is bigger then picture
+    gMeme.lines[lineIdx].offsetY += diff;
+}
+
 function addLine() {
     if (gMeme.lines.length >= 2) return //sopports only two lines by now
     var newLine = {
@@ -54,17 +85,24 @@ function addLine() {
         align: 'center',
         color: 'white',
         font: 'impact',
-        offsetY: gCanvas.height-80
+        stroke: true,
+        offsetY: gCanvas.height - 40
     }
     gMeme.lines.push(newLine);
-    gMeme.selectedLineIdx = gMeme.lines.length-1;
+    gMeme.selectedLineIdx = gMeme.lines.length - 1;
 }
 
-function moveline(diff) {
-    gMeme.selectedLineIdx += diff
-    if (gMeme.selectedLineIdx > gMeme.lines.length-1) gMeme.selectedLineIdx = 0;
-    if (gMeme.selectedLineIdx < 0) gMeme.selectedLineIdx = gMeme.lines.length-1;
+function deleteLine() {
+    var lineIdx = gMeme.selectedLineIdx;
+    gMeme.lines.splice(lineIdx, 1);
 }
+
+function selectLine(diff) {
+    gMeme.selectedLineIdx += diff
+    if (gMeme.selectedLineIdx > gMeme.lines.length - 1) gMeme.selectedLineIdx = 0;
+    if (gMeme.selectedLineIdx < 0) gMeme.selectedLineIdx = gMeme.lines.length - 1;
+}
+
 
 function getImages() {
     return gImgs;
--- js/memeController.js
+++ js/memeController.js
@@ -6,18 +6,23 @@ var gCtx = gCanvas.getContext('2d')
 
 function renderMeme() {
     var meme = getMeme();
-    // var selectedLine = meme.lines[meme.selectedLineIdx];
+    // render image:
     var img = document.querySelector(`.img${meme.selectedImgId}`);
     gCtx.drawImage(img, 0, 0)
     //render lines:
     meme.lines.forEach((line) => {
         gCtx.textAlign = line.align;
         var align = gCanvas.width / 2;
-        if (line.align === 'right') align = gCanvas.width-10; 
-        if (line.align === 'left') align = 10; 
-        gCtx.font = `${line.size}px ${line.font}`;
+        if (line.align === 'right') align = gCanvas.width - 10;
+        if (line.align === 'left') align = 10;
         gCtx.fillStyle = line.color;
+        gCtx.font = `${line.size}px ${line.font}`;
         gCtx.fillText(line.txt, align, line.offsetY)
+        if (line.stroke) {
+            gCtx.lineWidth = 2;
+            gCtx.strokeText(line.txt, align, line.offsetY)
+        }
+
     })
 }
 
@@ -37,9 +42,19 @@ function onChangeSize(arg) {
     renderMeme();
 }
 
+function onToggleStroke() {
+    toggleStroke()
+    renderMeme();
+}
+
 function onAlignChange(arg) {
     var align = arg.dataset.align;
     alignChange(align);
+    renderMeme();
+}
+
+function onChangeFont(font) {
+    changeFont(font);
     renderMeme();
 }
 
@@ -48,10 +63,30 @@ function onAddLine() {
     document.querySelector('.text-input').focus();
 }
 
-function onMoveline(diff) {
-    moveline(diff);    
+function onDeleteLine() {
+    deleteLine();
+    renderMeme();
+}
+
+function onLineSlect(diff) {
+    selectLine(diff);
     var line = getLine();
     var elTxtInput = document.querySelector('.text-input');
-    elTxtInput.value = line;
+    elTxtInput.value = line.txt;
     elTxtInput.focus();
+    document.querySelector('.input-font').value = line.font;
+}
+
+function onMoveline(diff) {
+    moveline(diff*10);
+    renderMeme();
+}
+
+function getCanvasHeight() {
+    return gCanvas.height
+}
+
+function onGalleryChoice() {
+    document.querySelector('.gallery').style = 'display: grid;'
+    document.querySelector('.meme-editor').style = 'display: none;'
 }
